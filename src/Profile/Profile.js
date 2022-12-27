e852cdea6048138bdb9a4a6538283044dc469bee
Olivia Adongo <akinyioliviatove@gmail.com>
2022-12-27 20:11:33 +0300

    Front-end

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Mood } from "@material-ui/icons";
+import "./Profile.css";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div className="profile">
+      <div className="profilecontent">
+        <img
+          className="catprofile"
+          src="https://i.pinimg.com/564x/83/34/e5/8334e5e5e0797ebc3441b95212b5b50b.jpg"
+          alt="I am a cat"
+        />
+<br/>
+        
+        
+<button class="button-31" role="button">LOGOUT</button>
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
